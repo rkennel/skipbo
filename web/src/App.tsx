e73cc3c2c55fe9d1d553d349de2e810a88559cdd
Ryan Kennel <rkennel@ford.com>
2020-04-22 19:01:41 -0400

    added router logic

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,25 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 import Login from "./Login";
 
-function App() {
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
-  const [game,setGame] = useState({id:"unknown"});
-  const [player,setPlayer] = useState({id:"unknown"});
+function App() {
+  const [game, setGame] = useState({ id: "unknown" });
+  const [player, setPlayer] = useState({ id: "unknown" });
 
   return (
     <div className="App">
-      <Login setGame={setGame} setPlayer={setPlayer}/>
+      <Router>
+        <Switch>
+          <Route path="/game">
+            <div>Let's Play a Game!</div>
+          </Route>
+          <Route path="/">
+            <Login setGame={setGame} setPlayer={setPlayer} />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
